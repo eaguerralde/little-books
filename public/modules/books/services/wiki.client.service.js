fb64e4597feb2cd5248592e559c44f82c38f3b19
Medion <Medion@Ague>
2014-09-27 19:56:48 +0100

    added books meanjs module and wiki angularjs service to it from scaffold

---
+++ public/modules/books/services/wiki.client.service.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('books').factory('Wiki', [
+	function() {
+		// Wiki service logic
+		// ...
+
+		// Public API
+		return {
+			someMethod: function() {
+				return true;
+			}
+		};
+	}
+]);
